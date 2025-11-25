945d7517f017db68c5039623bb1ea499f12cb988
សា រិតទីន <tamafiny354@gmail.com>
2025-11-25 21:42:48 +0700

    Update

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+/*=============== SWIPER POPULAR ===============*/
+const swiperpopular = new Swiper('.popluar__swiper', {
+  loop: true,
+  grabCursor:true,
+  spaceBetween:32,
+  slidePerView:'auto',
+  centeredSlides:'auto',
+
+  breakpoints:{
+    1150:{
+        spaceBetween: 80,
+    }
+  }
+});
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration: 2000,
+    deplay:300,
+    reset:true
+})
+
+sr.reveal(`.home__shape`,{origin:'bottom'})
+sr.reveal(`.home__coffee`,{delay: 1000 , distance:'200px' , duration:1500})
+sr.reveal(`.home__splash`,{delay: 1600 , scale:0 , duration:1500})
+sr.reveal(`.home__bean-1 .home__bean-2`,{delay: 2200 , scale:0 , duration:1500 , roteta:{z:180}})
+sr.reveal(`.home__ice-1 .home__ice-2`,{delay: 2600 , scale:0 , duration:1500 , roteta:{z:180}})
+sr.reveal(`.home__leaf`,{delay: 2800 , scale:0 , duration:1500 , roteta:{z:90}})
+sr.reveal(`.home__title`,{delay: 3500 ,})
+sr.reveal(`.home__data .home__sticker`,{delay: 4000 ,})
